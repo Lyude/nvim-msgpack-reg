d8dfcf1be9751e40647afc1d06596da246db914e
Lyude Paul <lyude@redhat.com>
2025-02-28 16:05:31 -0500

    Initial example commit
    This repository should be a very simple way of reproducing the issue
    described here:
    
    https://github.com/neovim/neovim/issues/31316
    
    Full instructions for doing this will be posted there.

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![allow(non_camel_case_types)]
+#![allow(non_upper_case_globals)]
+#![allow(unused)]
+mod bigmodule;
+
+// --- README ---
+// Following the steps I've included in the github issue after writing this, go into fn main()…
+
+fn main() {
+    println!("my name is car :)");
+    println!("you've heard my little song, now I have a constant value to show you!");
+
+    // …and type the following below:
+    //  bigmodule::B
+    // Backspace the B, and then type B again. Keep doing this repeatedly as quickly as you can, and
+    // neovim-gtk will reliably crash.
+
+}
